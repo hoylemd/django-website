ba1a48dbb5af6f23b8111ef4abb8f1f558058fc8
Michael D. Hoyle <hoylemd@gmail.com>
2018-04-27 22:37:58 -0400

    updte some stuff

--- michaelhoyle/settings.py
+++ michaelhoyle/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'michaelhoyle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
     }
 }
 
